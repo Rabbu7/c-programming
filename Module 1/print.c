12f95240bb36be54c427cf84fa51951e0985d504
Md. Fariz Uddin Rabbu <mdrabbu772@gmail.com>
2023-04-14 00:49:01 +0600

    Add files via upload

---
+++ Module 1/print.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello World\t\t\tNow I am a programmer");
+    // printf("Now I am a programmer");
+
+    return 0;
+}
+// \n= new line
+// \t= tab
+// hey guys 
+// what are you doing
---
+++ Module 1/variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int rabbu=100;
+    float risi=40.56;
+    char sun='y';
+    printf("%0.2f",risi);
+
+    return 0;
+}
