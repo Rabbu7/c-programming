1e5476e18ac18b015b57addf62fe0baff830bf68
Md. Fariz Uddin Rabbu <mdrabbu772@gmail.com>
2023-05-24 17:10:09 +0600

    Add files via upload

---
+++ Module 18__2D ARRAY/exact_row_column_print.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int row,col;
+    scanf("%d %d",&row,&col);
+    int a[row][col];
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    // int e=2;
+    // for(int i=0;i<col;i++)
+    // {
+    //     printf("%d ",a[e][i]);
+    // }
+    for(int i=0;i<row;i++)
+    {
+        printf("%d ",a[i][1]);
+    }
+    return 0;
+}
